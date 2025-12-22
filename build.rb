582ef3a19ce057ab936f7604d9deea0dfc3e6a1e
Tatsuto YAMAMOTO <me@laminne33569.net>
2025-12-22 21:57:58 +0900

    chore: ビルド設定にミスがあったので直した

--- build.rb
+++ build.rb
@@ -7,10 +7,10 @@ Dir.mkdir("built") unless Dir.exist?("built")
 versions = data['versions']
 versions.each do |version|
   `git clone -q --depth 1 -b #{version} https://github.com/mruby/mruby.git mruby-#{version}`
-    Dir.chdir("mruby-#{version}") do
-        `rake`
-        `cp build/host/bin/mruby ../built/mruby-#{version}`
-        `cp LICENSE ../built/mruby-#{version}-LICENSE`
-        `cp LEGAL ../built/mruby-#{version}-LEGAL`
-    end
+   Dir.chdir("mruby-#{version}") do
+     `rake -j5`
+     `cp build/host/bin/mruby ../built/mruby-#{version}`
+     `cp LICENSE ../built/mruby-#{version}-LICENSE`
+     `cp LEGAL ../built/mruby-#{version}-LEGAL`
+   end
 end
